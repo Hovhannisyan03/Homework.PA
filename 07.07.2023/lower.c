80246e09cfe7f3982db334c2b43291a99e9a4662
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2023-07-07 18:35:01 +0200

    Add files via upload

---
+++ 07.07.2023/lower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+     char sym;
+     int i = 1;
+     printf("Enter a uppercase:");
+     while(i){
+     	scanf("%c", &sym);
+	if(sym > 65 && sym <= 90)
+	{  sym += 32;
+	   printf("%c \n", sym); 
+	   break;
+	}else 
+	{
+	   printf("Invalid input:");
+	}
+     }
+     return 0;
+
+}
+
