693952a06237ace7f94a5d1bb7f5fcc4a6165d8d
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2023-07-07 18:27:35 +0200

    Add files via upload

---
+++ 07.07.2023/bigNumber.c
@@ -0,0 +1,28 @@
+// Գրեք ծրագիր, թույլ կտա օգտվողին մուտքագրել երկու թիվ, ստուգել և տպել դրանցից ամենամեծը:
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+
+    while(1)
+    {   printf("Enter a num1: ");
+        scanf("%d", &num1);
+        printf("Enter a num2: ");
+        scanf("%d", &num2);
+
+        if(num1 > num2) 
+        {
+            printf("The biggest number is %d", num1);
+        }else if(num1 < num2)
+        {
+            printf("The biggest number is %d", num2);
+        } else 
+        {
+            printf("The numbers are equal");
+        }
+        break;
+    }
+    return 0;
+}
