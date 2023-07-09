caf090e832a61fa216718c8d9ba947579779f1e0
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2023-07-09 21:22:26 +0400

    Update lower.c

--- 07.07.2023/lower.c
+++ 07.07.2023/lower.c
@@ -9,8 +9,9 @@ int main ()
      char sym;
      int i = 1;
      printf("Enter a uppercase:");
-     while(i){
-     	scanf("%c", &sym);
+     while(i)
+     {
+     	scanf(" %c", &sym);
 	if(sym >= 65 && sym <= 90)
 	{  sym += 32;
 	   printf("%c \n", sym); 
